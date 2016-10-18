273c07d51dbd2462d2539d9685f891d1a06d87b8
Shaun <scalagos0123@live.com>
2016-10-18 20:12:54 +0800

    added things to ignore

--- cashier-controller/model.php
+++ cashier-controller/model.php
@@ -1,5 +1,15 @@
 <?php
 
+class Model {
+    private $connect;
 
+    public function __construct() {
+        this.$connect = new mysqli("localhost:3306", "root", "", "flamingwings");
+    }
+
+    public function insertToDatabase($tablename) {
+        
+    }
+}
 
 ?>
