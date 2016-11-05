fb1b8906f6acfe975cddff3fa5cd8b5b54e2c450
Shaun <scalagos0123@live.com>
2016-11-05 14:32:28 +0800

    added ordershistory, navbar and sidebar

---
+++ OrdersHistory.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Orders History</title>
+        <?php include("templates/imports.php"); ?>
+    </head>
+    <body class="sidebar-mini skin-red">
+        <div class="wrapper">
+            <?php include("templates/navbar.php");?>
+            <?php include("templates/sidebar.php");?>
+
+            <section class="content-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
