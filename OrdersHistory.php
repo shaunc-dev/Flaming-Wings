5c84115bcecc784660e98fe24eeee10f8edf4941
Shaun <scalagos0123@live.com>
2016-11-05 20:49:56 +0800

    removed padding

--- OrdersHistory.php
+++ OrdersHistory.php
@@ -1,20 +1,59 @@
 <html>
     <head>
-        <title>Orders History</title>
+        <title>Flaming Wings | Orders History</title>
         <?php include("templates/imports.php"); ?>
+        <style>
+
+            .divider {
+                width: 100%;
+                border: 1px lightgray solid;
+            }
+
+            .orders {
+                margin-top: 20px;
+            }
+
+        </style>
     </head>
     <body class="sidebar-mini skin-red">
         <div class="wrapper">
             <?php include("templates/navbar.php");?>
             <?php include("templates/sidebar.php");?>
 
-            <section class="content-wrapper">
-                <div class="row">
-                    <div class="col-lg-12">
-                        
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <h1>Orders History</h1>
+                </section>
+                <section class="content">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="box box-danger box-solid">
+                                <div class="box-header">
+                                    <h4>Order #99</h4>
+                                </div>
+                                <div class="box-body no-padding">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Qty</th>
+                                                <th>Recipe name</th>
+                                                <th>Price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Flaming Wings 1</td>
+                                                <td>2.00</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
     </body>
 </html>
