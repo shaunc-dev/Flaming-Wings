8b93ca413cfc7b79960fe5a2eeb6a49b4e452e26
Shaun <scalagos0123@live.com>
2016-11-06 19:48:44 +0800

    modified sidebar php

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -4,13 +4,13 @@
 <section class="sidebar">
     <!-- Sidebar user panel -->
     <div class="user-panel">
-    <div class="pull-left image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-    </div>
-    <div class="pull-left info">
-        <p>Brooklyn Beckham</p>
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-    </div>
+        <div class="pull-left image">
+            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+            <p>Brooklyn Beckham</p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
     </div>
     
 
@@ -23,7 +23,7 @@
     <!--DASHBOARD-->
     <li class="treeview">
         <a href="http://localhost/Flaming-Wings/MAIN.php">
-        <i class="fa fa-dashboard"></i> <span>Dashboard</span> 
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span> 
         </a>
     </li>
 
@@ -34,17 +34,17 @@
     <!---RECIPE -->
     <li class="treeview">
         <a href="#">
-        <i class="fa fa-book"></i>
-        <span>Recipe</span>
-        <span class="label label-primary pull-right"></span>
+            <i class="fa fa-book"></i>
+            <span>Recipe</span>
+            <span class="label label-primary pull-right"></span>
         </a>
 
         <ul class="treeview-menu">
-        <li><a href="http://localhost/Flaming-Wings/SearchRecipe.php"><i class="fa fa-circle-o"></i> Search Recipe</a></li>
-        <li><a href="http://localhost/Flaming-Wings/AddRecipe.php"><i class="fa fa-circle-o"></i> Add Recipe</a></li>
-        <li><a href="http://localhost/Flaming-Wings/EditRecipe.php"><i class="fa fa-circle-o"></i> Edit Recipe</a></li>
-        <li><a href="http://localhost/Flaming-Wings/DeactivateRecipe.php"><i class="fa fa-circle-o"></i> Deactivate Recipe</a></li>
-        <li><a href="http://localhost/Flaming-Wings/reactivaterecipe.php"><i class="fa fa-circle-o"></i> Reactivate Recipe</a></li>
+            <li><a href="http://localhost/Flaming-Wings/SearchRecipe.php"><i class="fa fa-circle-o"></i> Search Recipe</a></li>
+            <li><a href="http://localhost/Flaming-Wings/AddRecipe.php"><i class="fa fa-circle-o"></i> Add Recipe</a></li>
+            <li><a href="http://localhost/Flaming-Wings/EditRecipe.php"><i class="fa fa-circle-o"></i> Edit Recipe</a></li>
+            <li><a href="http://localhost/Flaming-Wings/DeactivateRecipe.php"><i class="fa fa-circle-o"></i> Deactivate Recipe</a></li>
+            <li><a href="http://localhost/Flaming-Wings/reactivaterecipe.php"><i class="fa fa-circle-o"></i> Reactivate Recipe</a></li>
         </ul>
     </li>
 
@@ -60,11 +60,11 @@
         </a>
 
         <ul class="treeview-menu">
-        <li><a href="http://localhost/Flaming-Wings/SearchStock.php"><i class="fa fa-circle-o"></i> Search Stock</a></li>
-        <li><a href="http://localhost/Flaming-Wings/AddStock.php"><i class="fa fa-circle-o"></i> Add new Stock</a></li>
-        <li><a href="http://localhost/Flaming-Wings/ReplenishStock.php"><i class="fa fa-circle-o"></i> Replenish Stock</a></li>
-        <li><a href="http://localhost/Flaming-Wings/EditStock.php"><i class="fa fa-circle-o"></i> Edit Stock</a></li>
-        <li><a href="http://localhost/Flaming-Wings/WithdrawStock.php"><i class="fa fa-circle-o"></i> Withdraw Stock</a></li>
+            <li><a href="http://localhost/Flaming-Wings/SearchStock.php"><i class="fa fa-circle-o"></i> Search Stock</a></li>
+            <li><a href="http://localhost/Flaming-Wings/AddStock.php"><i class="fa fa-circle-o"></i> Add new Stock</a></li>
+            <li><a href="http://localhost/Flaming-Wings/ReplenishStock.php"><i class="fa fa-circle-o"></i> Replenish Stock</a></li>
+            <li><a href="http://localhost/Flaming-Wings/EditStock.php"><i class="fa fa-circle-o"></i> Edit Stock</a></li>
+            <li><a href="http://localhost/Flaming-Wings/WithdrawStock.php"><i class="fa fa-circle-o"></i> Withdraw Stock</a></li>
         </ul>
     </li>
 
@@ -81,9 +81,9 @@
         </a>
 
         <ul class="treeview-menu">
-        <li><a href="http://localhost/Flaming-Wings/InventoryReport.php"><i class="fa fa-circle-o"></i> Inventory Report</a></li>
-        <li><a href="http://localhost/Flaming-Wings/VerifyStock.php"><i class="fa fa-circle-o"></i>Stock Controller</a></li>
-        <li><a href="http://localhost/Flaming-Wings/MostSold.php"><i class="fa fa-circle-o"></i> Most sold order</a></li>
+            <li><a href="http://localhost/Flaming-Wings/InventoryReport.php"><i class="fa fa-circle-o"></i> Inventory Report</a></li>
+            <li><a href="http://localhost/Flaming-Wings/VerifyStock.php"><i class="fa fa-circle-o"></i>Stock Controller</a></li>
+            <li><a href="http://localhost/Flaming-Wings/MostSold.php"><i class="fa fa-circle-o"></i> Most sold order</a></li>
         </ul>
     </li>
 <!-- /.sidebar -->
@@ -98,7 +98,8 @@
 
 
         <ul class="treeview-menu">
-        <li><a href="http://localhost/Flaming-Wings/Conversion.php"><i class="fa fa-circle-o"></i>Conversion Table</a></li>
+            <li><a href="http://localhost/Flaming-Wings/Conversion.php"><i class="fa fa-circle-o"></i>Conversion Table</a></li>
         </ul>
     </li>
+    </section>
 </aside>
