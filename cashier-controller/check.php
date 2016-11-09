6a2c8993525a946bcce3408932bd053ae34de315
Shaun <scalagos0123@live.com>
2016-11-09 13:39:00 +0800

    added check

--- cashier-controller/check.php
+++ cashier-controller/check.php
@@ -7,11 +7,8 @@ if (!isset($_SESSION["type"], $_SESSION["user_id"])) {
     session_destroy();
     echo 'false';
 } else {
-<<<<<<< HEAD
-    if (!($_SESSION["type"] == 3) || !($_SESSION["type"] == 2)) {
-=======
+
     if (!$_SESSION["type"] == 3 || !$_SESSION["type"] == 2) {
->>>>>>> feature_cashier
         unset($_SESSION["type"], $_SESSION["user_id"]);
         session_destroy();
         echo 'false';
