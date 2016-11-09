0f41084b436571ce4cc2aa86b370cc4fea4c27df
Shaun <scalagos0123@live.com>
2016-11-09 13:11:09 +0800

    fixed type checking

--- cashier-controller/check.php
+++ cashier-controller/check.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["type"], $_SESSION["user_id"])) {
     session_destroy();
     echo 'false';
 } else {
-    if (!$_SESSION["type"] == 3) {
+    if (!($_SESSION["type"] == 3) || !($_SESSION["type"] == 2)) {
         unset($_SESSION["type"], $_SESSION["user_id"]);
         session_destroy();
         echo 'false';
