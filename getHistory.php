ac5c862651dc4a945176cb7bb119391901dc0f10
Shaun <scalagos0123@live.com>
2016-11-14 21:08:32 +0800

    testing getHistory

--- getHistory.php
+++ getHistory.php
@@ -1,10 +1,10 @@
 <?php
 
-include ("cashier-controller/connect.php");
+require ("cashier-controller/connect.php");
 
 function queryCondition($endDate = "") {
     if ($endDate == "") {
-        return " = ?";
+        return " = ? ";
     } else {
         return " between ? and ? ";
     }
@@ -13,26 +13,48 @@ function queryCondition($endDate = "") {
 // if (isset($_POST["start"], $_POST["end"])) {
 
     $data = array();
-    $data["orders"] = array();
 
     // $startDate = $_POST["start"];
     // $endDate = $_POST["end"];
     $startDate = "2016-11-04";
-    $endDate = null;
+    $endDate = "2016-11-09";
 
-    $orders_query = $connect->prepare("select r.recipe_name, sum(sd.qty) as qty, (r.price * sd.qty) as price from sales s, sales_details sd, recipe r where sd.sales_details_id = s.sales_id && sd.recipe_id = r.recipe_id && s.dtSales".queryCondition(). "group by recipe_name, sales_id order by s.dtSales");
+    $sales_query = $connect->prepare("select * from sales where dtSales".queryCondition($endDate));
+    $orders_query = $connect->prepare("select sd.sales_id, r.recipe_name, sum(sd.qty) as qty, sum(r.price * sd.qty) as price 
+    from sales s, sales_details sd, recipe r 
+    where sd.sales_details_id = s.sales_id && sd.recipe_id = r.recipe_id && sd.sales_id = ?
+    group by recipe_name;");
 
-    if (!isset($endDate)) {
-        $orders_query->bind_param("s", $startDate);
+    if ($endDate == "") {
+        $sales_query->bind_param("s", $startDate);
     } else {
-        $orders_query->bind_param("ss", $startDate, $endDate);
+        $sales_query->bind_param("ss", $startDate, $endDate);
     }
 
-    $orders_query->execute();
-    $orders_result = $orders_query->get_result();
+    $sales_query->execute();
+    $sales_result = $sales_query->get_result();
 
-    foreach ($orders_result->fetch_assoc() as $row) {
-        array_push($data["orders"], new ArrayObject($row));
+    while ($sales_row = $sales_result->fetch_array(MYSQLI_ASSOC)) {
+        $object = new ArrayObject();
+        $object["id"] = $sales_row["sales_id"];
+        $object["date"] = $sales_row["dtSales"];
+        $object["total"] = $sales_row["total"];
+        $object["orders"] = array();
+
+        $orders_query->bind_param("i", $sales_row["sales_id"]);
+        $orders_query->execute();
+        $orders_result = $orders_query->get_result();
+
+        while ($orders_row = $orders_result->fetch_array(MYSQLI_ASSOC)) {
+            $orders = new ArrayObject();
+            $orders["qty"] = $orders_row["qty"];
+            $orders["recipe_name"] = $orders_row["recipe_name"];
+            $orders["price"] = $orders_row["price"];
+
+            array_push($object["orders"], $orders);
+        }
+
+        array_push($data, $object);
     }
 
     echo json_encode($data);
