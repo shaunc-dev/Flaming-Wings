143d30fce0fd53823235158c9388f534efb89a6e
Shaun <scalagos0123@live.com>
2016-11-14 23:45:39 +0800

    getHistory back end done

--- getHistory.php
+++ getHistory.php
@@ -2,40 +2,22 @@
 
 require ("cashier-controller/connect.php");
 
-function queryCondition($endDate = "") {
-    if ($endDate == "") {
-        return " = ? ";
-    } else {
-        return " between ? and ? ";
-    }
-}
-
-// if (isset($_POST["start"], $_POST["end"])) {
+if (isset($_POST["start"], $_POST["end"])) {
 
     $data = array();
 
-    // $startDate = $_POST["start"];
-    // $endDate = $_POST["end"];
-    $startDate = "2016-11-14";
-    $endDate = "";
+    $startDate = $connect->real_escape_string($_POST["start"]);
+    $endDate = $connect->real_escape_string($_POST["end"]);
 
-    $sales_query = $connect->prepare("select * from sales where date(dtSales)".queryCondition($endDate));
-    $orders_query = $connect->prepare("select sd.sales_id, r.recipe_name, sum(sd.qty) as qty, sum(r.price * sd.qty) as price 
-    from sales s, sales_details sd, recipe r 
-    where sd.sales_details_id = s.sales_id && sd.recipe_id = r.recipe_id && sd.sales_id = ?
-    group by recipe_name;");
-
-    echo "select * from sales where dtSales".queryCondition($endDate);
-    echo "<br>";
+    $sales_query = "";
 
     if ($endDate == "") {
-        $sales_query->bind_param("s", $startDate);
+        $sales_query = "select * from sales where date(dtSales) = '".$startDate."'";
     } else {
-        $sales_query->bind_param("ss", $startDate, $endDate);
+        $sales_query = "select * from sales where date(dtSales) between '".$startDate."' and '".$endDate."'";
     }
 
-    $sales_query->execute();
-    $sales_result = $sales_query->get_result();
+    $sales_result = $connect->query($sales_query);
 
     while ($sales_row = $sales_result->fetch_array(MYSQLI_ASSOC)) {
         $object = new ArrayObject();
@@ -44,11 +26,15 @@ function queryCondition($endDate = "") {
         $object["total"] = $sales_row["total"];
         $object["orders"] = array();
 
-        $orders_query->bind_param("i", $sales_row["sales_id"]);
-        $orders_query->execute();
-        $orders_result = $orders_query->get_result();
+        $orders_query = "select sd.sales_id, r.recipe_name, sum(sd.qty) as qty, sum(r.price * sd.qty) as price 
+        from sales s, sales_details sd, recipe r 
+        where sd.sales_id = s.sales_id && sd.recipe_id = r.recipe_id && sd.sales_id = '".$sales_row["sales_id"]."'
+        group by recipe_name";
+
+        $orders_result = $connect->query($orders_query);
 
         while ($orders_row = $orders_result->fetch_array(MYSQLI_ASSOC)) {
+
             $orders = new ArrayObject();
             $orders["qty"] = $orders_row["qty"];
             $orders["recipe_name"] = $orders_row["recipe_name"];
@@ -58,13 +44,11 @@ function queryCondition($endDate = "") {
         }
 
         array_push($data, $object);
+
     }
 
     echo json_encode($data);
-
-    
-// } else {
-
-// }
+      
+}
 
 ?>
