99fe3bc396e9a70432174bb9c62ceb71fcb532ab
Shaun <scalagos0123@live.com>
2016-11-25 14:33:48 +0800

    initial file for summary.php

---
+++ summary.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Summary of Sales</title>
+        <?php include ('templates/imports.php');
+    </head>
+    <body>
+        <div class="wrapper">
+            <?php include('templates/navbar.php');
+            <?php include('templates/sidebar.php');
+            
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <h1>Summary</h1>
+                </section>
+                <section class="content">
+                    
+                </section>
+            </div>
+        </div>
+    </body>
+</html>
