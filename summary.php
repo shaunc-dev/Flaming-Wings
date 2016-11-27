03c1bb32eef5d8dfba292e6b4573de9d1c204545
Shaun <scalagos0123@live.com>
2016-11-27 17:50:52 +0800

    added comparison

--- summary.php
+++ summary.php
@@ -3,6 +3,31 @@
     <head>
         <title>Summary of Sales</title>
         <?php include ('templates/imports.php'); ?>
+        <style>
+
+            .selection {
+                /*margin-top: 20px;*/
+                text-align: left;
+            }
+
+            .selection > a {
+                display: block;
+                padding: 4px;
+                color: black;
+                cursor: pointer;
+            }
+
+            .selection > a.active, .selection > a.active:hover {
+                color: white;
+                background: rgba(225, 72, 53, 1.0);
+            }
+
+            .selection > a:hover {
+                color: white;
+                background: rgba(249, 151, 141, 1.0);
+            }
+
+        </style>
     </head>
     <body class="sidebar-mini skin-red">
         <div class="wrapper">
@@ -15,10 +40,10 @@
                 </section>
                 <section class="content">
                     <div class="row">
-                        <div class="col-xs-12">
-                            <div class="box box-primary">
-                                <div class="box-header">
-                                    <h3 class="box-title">Performance chart</h3>
+                        <div class="col-xs-12 col-md-9">
+                            <div class="box box-danger">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">Sales Performance</h3>
                                     <small>Comparing today and yesterday</small>
                                 </div>
                                 <div class="box-body">
@@ -26,9 +51,24 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-xs-12 col-md-3">
+                            <div class="box box-danger">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">Choose a comparison</h3>
+                                </div>
+                                <div class="box-body">
+                                    <div class="selection">
+                                        <a data-value="now" class="active">Today</a>
+                                        <a data-value="lweek">Last week</a>
+                                        <a data-value="lmonth">Last month</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </div>
         </div>
+         <script src="dist/js/orders-history.js"></script>
     </body>
 </html>
