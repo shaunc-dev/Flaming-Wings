7eb1c968fe32edcec44a24cd2f441891a81742b8
Shaun <scalagos0123@live.com>
2016-11-27 13:24:18 +0800

    initial summary page

--- summary.php
+++ summary.php
@@ -4,7 +4,7 @@
         <title>Summary of Sales</title>
         <?php include ('templates/imports.php'); ?>
     </head>
-    <body class="skin-red">
+    <body class="sidebar-mini skin-red">
         <div class="wrapper">
             <?php include('templates/navbar.php'); ?>
             <?php include('templates/sidebar.php'); ?>
@@ -14,7 +14,11 @@
                     <h1>Summary</h1>
                 </section>
                 <section class="content">
-                    
+                    <div class="row">
+                        <div class="col-xs-12">
+                            
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
