32f53f9681306012ab1a418c43fc990478d4b37a
Shaun <scalagos0123@live.com>
2016-12-01 12:55:22 +0800

    adding printing of sales report

---
+++ generate-sales-report.php
@@ -0,0 +1,18 @@
+<?php include ("dbconnection.php"); session_start(); ?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Flaming Wings | Sales Report</title>
+        <?php include("templates/imports.php"); ?>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-xs-12">
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
