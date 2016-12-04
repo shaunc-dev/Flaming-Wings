e2fd4b3ea5d476d0d4a40d4b93b897bd3a5a9cb6
Shaun <scalagos0123@live.com>
2016-12-04 21:06:55 +0800

    fixed session and price

--- NewlyAddedRecipe.php
+++ NewlyAddedRecipe.php
@@ -21,7 +21,7 @@ echo $_POST["recipe_type"];
 // echo "qtyname: ".$_POST["qtyname"][$i]."<br>";
 // echo "<br>";
 
-$query = "INSERT recipe (recipe_name, recipe_typeid) VALUES('".$_POST["recipe_name"]."', '".$_POST["recipe_type"]."')";
+$query = "INSERT recipe (recipe_name, recipe_typeid, price) VALUES('".$_POST["recipe_name"]."', '".$_POST["recipe_type"]."', '{$_POST["price"]}')";
 $result = mysqli_query($connect, $query) or die (mysqli_error($connect));
 
 $id = mysqli_insert_id($connect); 
